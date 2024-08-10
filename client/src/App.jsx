f473f40f7b580b80c0844b21ecbf24c843399351
psstoykov <145485240+psstoykov@users.noreply.github.com>
2024-08-10 12:36:31 +0200

    convert html to jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+
 function App() {
-    return <></>;
+    return (
+        <div id="box">
+            <Header />
+            <main id="main-content">
+                <Home />
+            </main>
+        </div>
+    );
 }
 
 export default App;

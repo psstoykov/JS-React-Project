459a72f97dc910b92fdf9df56acc8bb83ad41ceb
psstoykov <145485240+psstoykov@users.noreply.github.com>
2024-08-10 15:53:49 +0200

    load catalog content

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,51 +1,27 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import * as gamesAPI from "../../api-service/game-api";
+import CatalogItem from "./catalog-item/CatalogItem";
 
 export default function Catalog() {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
-        gamesAPI.getAll().then((result) => setGames(result));
-        console.log(games);
+        gamesAPI.getAll().then((result) => {
+            setGames(result);
+        });
+
         //TODO change to async function
     }, []);
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* Display div: with information about every game (if any) */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
-            {/* Display paragraph: If there is no games  */}
-            <h3 className="no-articles">No articles yet</h3>
+
+            {games.length > 0 ? (
+                games.map((game) => <CatalogItem key={game._id} {...game} />)
+            ) : (
+                <h3 className="no-articles">No games yet</h3>
+            )}
         </section>
     );
 }
---
+++ client/src/components/catalog/catalog-item/CatalogItem.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+export default function CatalogItem({ id, title, category, imageURL }) {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={imageURL} />
+                <h6>{category}</h6>
+                <h2>{title}</h2>
+                <Link to="#" className="details-button">
+                    {/* TODO fix link to details */}
+                    Details
+                </Link>
+            </div>
+        </div>
+    );
+}

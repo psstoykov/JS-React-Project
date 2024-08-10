3fe9de427088839d30db8239be615fe13fcabe5e
psstoykov <145485240+psstoykov@users.noreply.github.com>
2024-08-10 15:00:21 +0200

    setup server

---
+++ client/src/api-service/game-api.js
@@ -0,0 +1,5 @@
+import { requester } from "./requester"
+
+const BASE_URL = 'http://localhost:3030/jsonstore/games';
+
+export const getAll = () => requester.get(BASE_URL);
--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import * as gamesAPI from "../../api-service/game-api";
+
 export default function Catalog() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gamesAPI.getAll().then((result) => setGames(result));
+        console.log(games);
+        //TODO change to async function
+    }, []);
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
